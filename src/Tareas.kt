b6f2f02707b938e1aa07a522932d46260272caec
hebrabo <hebrabo@gmail.com>
2025-10-09 20:12:09 +0200

    Práctica 2 completa: To-Do App en Kotlin

---
+++ src/Tareas.kt
@@ -0,0 +1,18 @@
+class Tareas(
+    val id: Int,
+    val titulo: String,
+    val descripcion: String,
+    var completada: Boolean = false
+) {
+    fun marcarComoCompletada() {
+        completada = true
+    }
+
+    fun mostrarInfo()  {
+        val estado = if (completada) "[✔]" else "[ ]"
+        println("$estado $id - $titulo ($descripcion)")
+    }
+}
+
+
+
