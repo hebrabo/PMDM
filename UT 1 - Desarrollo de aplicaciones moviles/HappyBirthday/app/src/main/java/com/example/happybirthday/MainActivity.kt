9c8ba8a48181999d094eae52686c7d01bd6ab89e
hebrabo <hebrabo@gmail.com>
2025-10-22 07:10:27 +0200

    Agregar otro elemento de texto

--- UT 1 - Desarrollo de aplicaciones moviles/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ UT 1 - Desarrollo de aplicaciones moviles/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -30,18 +30,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier : Modifier = Modifier){
+fun GreetingText(message: String, from: String, modifier : Modifier = Modifier){
 Text(
     text = message,
     fontSize = 100.sp,
     lineHeight = 116.sp,
     )
+Text(
+    text = from,
+    fontSize = 36.sp
+)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Helena!")
+        GreetingText(message = "Happy Birthday Aixa!" , from = "From Helena")
     }
 }
