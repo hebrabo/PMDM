07054948bad3da1183c2cff8022286db41d3f34d
hebrabo <hebrabo@gmail.com>
2025-12-16 08:48:02 +0100

    Ejercicio 6: Enviar notificaciones periódicas mientras el usuario está activo.
    Qué practicar:
    - while (isActive)
    - cancelación de corrutinas
    - Job

---
+++ UT 1 - Desarrollo de aplicaciones moviles/PMDM01_16_PracticarCorrutinas/src/T6_SistemaNotificaciones.kt
@@ -0,0 +1,48 @@
+import kotlinx.coroutines.*
+
+/*
+Idea: Enviar notificaciones periódicas mientras el usuario está activo.
+Qué practicar:
+- launch
+- while (isActive)
+- Job.cancel()
+ */
+
+fun main() = runBlocking {
+    println("--- Sistema Iniciado ---")
+
+    // 1. INICIO
+    val servicioJob = launch {
+        enviarNotificaciones()
+    }
+
+    // 2. USO (Simulación)
+    simularActividadUsuario()
+
+    // 3. CIERRE
+    println("Logout: Usuario cerrando sesión...")
+    servicioJob.cancelAndJoin() // Apagamos el servicio
+
+    println("--- Sistema Apagado ---")
+}
+
+// ---------------- FUNCIONES ----------------
+
+// Esto crea un entorno de corrutina dentro de la función.
+// Ventaja: Nos da acceso directo a variables de control como 'isActive'.
+suspend fun enviarNotificaciones() = coroutineScope {
+    println("Servicio de notificaciones activado")
+
+    // 'isActive' está conectado directamente al 'servicioJob' del main.
+    // Si se llama a .cancel(), esta variable cambia automáticamente a 'false'
+    // y el bucle se rompe.
+    while (isActive) {
+        println("Ping: Tienes una nueva notificación")
+        delay(1000)
+    }
+}
+
+suspend fun simularActividadUsuario() {
+    println("Usuario leyendo noticias...")
+    delay(3500) // Simula actividad bloqueando el flujo principal 3.5s
+}
